8f0a412b6407b030cde2833c6c8b609495c88f9d
Wasim Elias Basel Qassem <epwebq@vlsi-ria87.technion.ac.il>
2025-06-21 17:17:15 +0300

    [ID][IMM_GEN] fix imm generator and add tb

--- ID_Unit/IMMEDIATE_GENERATOR/IMM_GENERATOR.sv
+++ ID_Unit/IMMEDIATE_GENERATOR/IMM_GENERATOR.sv
@@ -6,18 +6,50 @@
  * Description   :
  *------------------------------------------------------------------------------*/
 
-module IMM_GENETOR #(
-	IMM_WIDTH          		  = `IMM_WIDTH,
+`timescale 1ns/1ns
+
+module IMM_GENERATOR #(
 	ARCH_REG_NUM_WIDTH 		  = `ARCH_REG_NUM_WIDTH,
 	GENERATED_IMMEDIATE_WIDTH = `REG_VAL_WIDTH
 ) (
-	input  [IMM_WIDTH-1:0] 					immediate_input,
+	input  [31:0] 							Instruction_code,
 	output [GENERATED_IMMEDIATE_WIDTH-1:0] 	generated_immediate
 );
 
- localparam SIGN_EXTENSION_WIDTH = GENERATED_IMMEDIATE_WIDTH - IMM_WIDTH;
+	opcode_t  	opcode 	  ;
+	bit [31:0]	immediate ;	
+	bit [4:0]   imm_width ;
+	bit 	  	twelve_bit;
+
+	always_comb
+		begin
+			
+		opcode = Instruction_code[`OPCODE_WIDTH-1:0];
+		
+			if(opcode == R_type) begin
+				immediate = 0;
+				imm_width = 1;
+			end
+			
+			else if(opcode == I_type_load || opcode == I_type_arth) begin
+				immediate = Instruction_code[31:20];
+				imm_width = 12;
+			end
+			
+			else if(opcode == S_type) begin
+				immediate = {Instruction_code[31:25] , Instruction_code[11:7] };
+				imm_width = 12;
+			end
+			
+			else if(opcode == SB_type) begin
+				immediate = {Instruction_code[31],Instruction_code[7],Instruction_code[30:25],Instruction_code[11:8],1'b0};
+				imm_width = 13;
+			end
+		end
  
- assign generated_immediate = {{(SIGN_EXTENSION_WIDTH){immediate_input[IMM_WIDTH-1]}} , immediate_input };
+ 
+ assign generated_immediate = {{(19){twelve_bit}},twelve_bit, immediate[11:0]};
+ assign twelve_bit = (imm_width == 13)? Instruction_code[31] : immediate[11] ;
 
 
 endmodule
---
+++ ID_Unit/IMMEDIATE_GENERATOR/IMM_GENERATOR_TB.sv
@@ -0,0 +1,91 @@
+/*------------------------------------------------------------------------------
+ * File          : IMM_GENERATOR_TB.sv
+ * Project       : RTL
+ * Author        : epwebq
+ * Creation date : Jun 21, 2025
+ * Description   :
+ *------------------------------------------------------------------------------*/
+
+`timescale 1ns/1ns
+
+module IMM_GENERATOR_TB #() ();
+	
+	//clk
+	logic 						clk;
+	
+	//DUT inputs
+	logic [31:0]				Instruction_code;
+	
+	//DUT outputs
+	logic [31:0]				generated_immediate;
+	
+	
+	//DUT instantiation
+	IMM_GENERATOR imm_gen (
+		
+		//input
+		.Instruction_code	(Instruction_code),
+		//output
+		.generated_immediate(generated_immediate)
+	);
+	
+	
+	//clk
+	initial begin
+		clk = 1'b0; // Ensure clk is explicitly 0 at time 0
+	end
+	always #20 clk = ~clk ;	
+	
+	//drive inputs for the DUT
+	initial
+		begin
+			//inital values      								------- IMM = 0 -------
+			Instruction_code[`OPCODE_WIDTH-1:0] = R_type;
+			Instruction_code[11:7] 	= 7;
+			Instruction_code[31:20] = 66;
+			//      											------- IMM = 24 -------
+		#40 Instruction_code[`OPCODE_WIDTH-1:0] = I_type_load;
+			Instruction_code[11:7] 	= 11;
+			Instruction_code[31:20] = 24;
+			//      											------- IMM = 44 -------
+		#40 Instruction_code[`OPCODE_WIDTH-1:0] = S_type;
+			Instruction_code[11:7] 	= 5'b01100;
+			Instruction_code[31:20] = 12'b100100;
+			//      											------- IMM = 2152 -------
+		#40 Instruction_code[`OPCODE_WIDTH-1:0] = SB_type;
+			Instruction_code[11:7] 	= 5'b01001;
+			Instruction_code[31:20] = 12'b000001111000;
+			//      											------- IMM = -4 -------
+		#40 Instruction_code[`OPCODE_WIDTH-1:0] = I_type_load;
+			Instruction_code[11:7] 	= 5'b101;
+			Instruction_code[31:20] = 12'b111111111100;
+			//      											------- IMM = 12 -------
+		#40 Instruction_code[`OPCODE_WIDTH-1:0] = S_type;
+			Instruction_code[11:7] 	= 5'b01100;
+			Instruction_code[31:20] = 16;
+			//      											------- IMM = 5 -------
+		#40 Instruction_code[`OPCODE_WIDTH-1:0] = I_type_arth;
+			Instruction_code[11:7] 	= 2;
+			Instruction_code[31:20] = 5;
+			//													------- IMM = -20 -------
+		#40 Instruction_code[`OPCODE_WIDTH-1:0] = SB_type;
+			Instruction_code[11:7] 	= 5'b01101;
+			Instruction_code[31:20] = 12'b111111100000;
+
+		end
+
+	//end test after 500ns
+	initial 
+		#500 $finish;
+	
+	//Setting Up waveform
+	initial
+		begin
+			$dumpfile("IMM_GENERATOR_TB_output_wave.vcd");
+			$dumpvars(0,IMM_GENERATOR_TB);
+		end
+		
+	
+	
+	
+endmodule
